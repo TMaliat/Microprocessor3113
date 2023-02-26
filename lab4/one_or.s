cbce824ada5bb03f05ea96ae28de2cc2a04b1acf
Tanzeem <95355941+TMaliat@users.noreply.github.com>
2023-02-27 00:43:20 +0600

    Create one_or.s

---
+++ lab4/one_or.s
@@ -0,0 +1,10 @@
+		AREA test, CODE, READONLY
+		ENTRY ; starting point of the code execution
+		EXPORT main ; the declaration of identifier main
+main ; address of the main function
+	; User code starts from the next line
+	MOV r1, #0x1234 ; store some arbitrary numbers
+	MOV r2, #0xABCD
+	ORR r0, r1, r2 ; add the values in r0 and r1 and store the result in r2
+Stop B Stop ; Endless loop
+	END ; End of the program, matched with ENTRY keyword
